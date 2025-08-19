f6744e1d6f8a82e8d1113da2ed6e42b932ee4782
jeffersonrolino <jefferson.rolino@gmail.com>
2025-08-19 02:42:29 -0300

    initial spring boot project

---
+++ src/test/java/jeffersonrolino/com/github/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package jeffersonrolino.com.github.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
